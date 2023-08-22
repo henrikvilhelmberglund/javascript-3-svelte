0f16f11e78633047b0fc777dfe2dd636d64e7a8d
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-08-22 15:17:08 +0200

    feat: :sparkles: finish exercise 2

---
+++ src/routes/01lektion1/02/+page.svelte
@@ -0,0 +1,59 @@
+<script>
+	import Profile from "./Profile.svelte";
+
+	const persons = [
+		{
+			firstName: "Henrik",
+			lastName: "Berglund",
+			age: "35",
+			hobby: "Playing guitar",
+		},
+		{
+			firstName: "Bat",
+			lastName: "Man",
+			age: "45",
+			hobby: "Being a super hero",
+		},
+		{
+			firstName: "Super",
+			lastName: "Man",
+			age: "25",
+			hobby: "Feeling super",
+		},
+		{
+			firstName: "Mr",
+			lastName: "Duck",
+			age: "20",
+			hobby: "Eating bread",
+		},
+		{
+			firstName: "Java",
+			lastName: "Script",
+			age: "50",
+			hobby: "Programming",
+		},
+	];
+
+	let darkMode = false;
+</script>
+
+<!-- without loop -->
+<!-- <Profile person={persons[0]}></Profile>
+<Profile person={persons[1]}></Profile>
+<Profile person={persons[2]}></Profile>
+<Profile person={persons[3]}></Profile>
+<Profile person={persons[4]}></Profile> -->
+
+<svelte:body />
+
+<main class={darkMode ? "dark" : ""}>
+	<section class="m-2 dark:bg-black dark:text-white">
+		{#each persons as person}
+			<Profile {person} />
+		{/each}
+		<button on:click={() => (darkMode = !darkMode)}>Toggle darkmode</button>
+	</section>
+</main>
+
+<style>
+</style>
---
+++ src/routes/01lektion1/02/Profile.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let person = {};
+	let showMore = false;
+</script>
+
+	<h3 class="inline">{person.firstName}</h3>
+	{#if showMore}
+		<h3 class="inline">{person.lastName}</h3>
+		<span>({person.age})</span>
+		<h4>
+			hobby:
+			{person.hobby}
+		</h4>
+	{/if}
+	<button class="block" on:click={() => (showMore = !showMore)}>
+		{showMore ? "Show less" : "Show more"}
+	</button>
+
+<style>
+</style>
