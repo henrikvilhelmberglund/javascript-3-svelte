5313945d30d120e3e7d0ef2ad6ba6afe6aeb8011
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-09-04 17:33:25 +0200

    feat: :sparkles: add streaks tracker

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,7 @@
 		{ link: "/02lektion2", display: "Lektion 2" },
 		{ link: "/03lektion3", display: "Lektion 3" },
 		{ link: "/04lektion4", display: "Lektion 4" },
+		{ link: "/05lektion5", display: "Lektion 5" },
 	];
 </script>
 
---
+++ src/routes/05lektion5/Parent.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { getContext } from "svelte";
+	import Child from "./Child.svelte";
+	const streaks = getContext("streaks");
+</script>
+
+<div class="mx-0 flex flex-col items-center">
+	{#each $streaks as streak}
+		<Child {streak} />
+	{/each}
+</div>
+
+<style>
+</style>
